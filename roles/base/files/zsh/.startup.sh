68a0a673242bd3c9227484a00b30733cd5682a46
Wickes Wong <27881570+wickes1@users.noreply.github.com>
2022-09-25 11:02:53 +0800

    update: startup script

--- roles/base/files/zsh/.startup.sh
+++ roles/base/files/zsh/.startup.sh
@@ -1,2 +1,21 @@
+
+# === Setup Environment ===
+# load zshrc
+source $HOME/.zshrc
+
+# set extra env
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+export DISPLAY=:0.0
+
+# === Startup Command ===
+# welcome words
+notify-send 'hello wolrd'
+
+# auto hide mouse cursor
+unclutter -idle 10 &
+
 # go app update
 gup update
+
+# goodby words
+notify-send 'goodbye wolrd'
