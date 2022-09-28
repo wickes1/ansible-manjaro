39cb33166b8b62741d8301b1dbd0f8b2694d39f8
Wickes1 <27881570+wickes1@users.noreply.github.com>
2022-09-28 10:00:08 +0800

    update: scripts gqlgeninit

--- roles/base/files/zsh/.scripts.sh
+++ roles/base/files/zsh/.scripts.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
-function dg(){
-  datamodel-codegen --input $1 --input-file-type $2 --output $3
+function dg() {
+	datamodel-codegen --input "$1" --input-file-type "$2" --output "$3"
 }
 
 # replace all occurance in current direcctory
-function sdd(){
-  sd $1 $2 $(fd --type file)
+function sdd() {
+	sd "$1" "$2" "$(fd --type file)"
 }
 
+function gqlgeninit() {
+	#!/bin/zsh
+	# file: gqlgen-gen.sh
+	# usage: chmod+x gqlgen-gen.sh && ./gqlgen-gen.sh
+
+	# variable
+  echo -n "Enter go module name: "
+  read MODULE
+	# read -k "Enter go module name: " MODULE
+
+	# module init
+  echo "$MODULE"
+	mkdir "$MODULE"
+	cd "$MODULE" || exit
+	go mod init "$MODULE"
+	go get -u github.com/99designs/gqlgen
+	go run github.com/99designs/gqlgen init
+
+	# create tool dependencices
+	echo "Creating tool dependencies"
+	mkdir tools
+	cat <<EOF >>tools/tools.go
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+_ "github.com/99designs/gqlgen"
+)
+EOF
+
+	# go generate ./...
+	echo "Seding go generate"
+	sed -i '/type Resolver struct{}/i \
+  // \
+  //go:generate go run github.com/99designs/gqlgen generate' graph/resolver.go
+	go mod tidy
+	go generate ./...
+
+	# tips
+	echo "Run 'go generate ./...' to generate new model"
+	echo "Run 'PORT=4000 go run ./server.go' to specify port run"
+}
